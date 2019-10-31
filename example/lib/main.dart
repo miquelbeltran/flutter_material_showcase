e9c99af1cb09c85376405cc22e8d7cffccb05beb
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2019-10-31 14:41:45 +0100

    empty project

---
+++ example/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_material_showcase/flutter_material_showcase.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material Design Showcase'),
+      ),
+      body: ListView(children: <Widget>[
+        MaterialShowcase(),
+      ],),
+    );
+  }
+}
---
+++ lib/flutter_material_showcase.dart
@@ -0,0 +1,12 @@
+library flutter_material_showcase;
+
+import 'package:flutter/material.dart';
+
+class MaterialShowcase extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
